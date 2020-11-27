274167a0def8ccb94dfe024b4b04c73cf6e99904
lxucs <leonxu624@gmail.com>
2020-11-27 00:11:40 -0500

    Fixed

--- setup_data.sh
+++ setup_data.sh
@@ -16,9 +16,9 @@ dlx $conll_url/test conll-2012-test-key.tar.gz
 dlx $conll_url/test conll-2012-test-official.v9.tar.gz
 
 dlx $conll_url conll-2012-scripts.v3.tar.gz
-dlx http://conll.cemantix.org/download reference-coreference-scorers.v8.01.tar.gz
+# dlx http://conll.cemantix.org/download reference-coreference-scorers.v8.01.tar.gz
 
-bash conll-2012/v3/scripts/skeleton2conll.sh -D $ontonotes_path/data/files/data $data_dir/conll-2012
+bash $data_dir/conll-2012/v3/scripts/skeleton2conll.sh -D $ontonotes_path/data/files/data $data_dir/conll-2012
 
 function compile_partition() {
     rm -f $2.$5.$3$4
