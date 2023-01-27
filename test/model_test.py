9b1a90e1228a1c88aac2f30b518937ce471b6e52
KyN76 <simon.dcmbl@gmail.com>
2023-01-27 17:01:01 +0100

    test : dummy test

--- test/model_test.py
+++ test/model_test.py
@@ -10,12 +10,21 @@ class TestModel(unittest.TestCase):
         antecedent_idx = [[1, 2, 3, 4], [5, 6, 7, 8]]
         antecedent_scores = [[0, 20, 50, 30, 40], [0, 60, 30, 80, 40]]
         k = 3
-        
-        k_best_antecedents, k_best_antecedent_scores = model.CorefModel.get_k_best_predicted_antecedents(antecedent_idx, antecedent_scores, k)
-
         expected_k_best_antecedent_scores = torch.tensor([[50, 40, 30], [80, 60, 40]])
         expected_k_best_antecedents = torch.tensor([[2, 4, 3], [7, 5, 8]])
         
+        k_best_antecedents, k_best_antecedent_scores = model.CorefModel.get_k_best_predicted_antecedents(antecedent_idx, antecedent_scores, k)
+        self.assertTrue(torch.equal(expected_k_best_antecedent_scores, k_best_antecedent_scores))
+        self.assertTrue(torch.equal(expected_k_best_antecedents, k_best_antecedents))
+
+    def test_get_k_best_predicted_antecedents_dummy(self):
+        antecedent_idx = [[1, 2, 3, 4], [5, 6, 7, 8]]
+        antecedent_scores = [[0, 20, 50, 30, 40], [0, 60, -10, 80, -20]]
+        k = 3
+        expected_k_best_antecedent_scores = torch.tensor([[50, 40, 30], [80, 60, 0]])
+        expected_k_best_antecedents = torch.tensor([[2, 4, 3], [7, 5, -1]])
+
+        k_best_antecedents, k_best_antecedent_scores = model.CorefModel.get_k_best_predicted_antecedents(antecedent_idx, antecedent_scores, k)
         self.assertTrue(torch.equal(expected_k_best_antecedent_scores, k_best_antecedent_scores))
         self.assertTrue(torch.equal(expected_k_best_antecedents, k_best_antecedents))
 
